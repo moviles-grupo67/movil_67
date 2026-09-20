a7d61e7bab08af39510011ae5b72f17c4336d25e
zequisanchez <ezequieledu.san@gmail.com>
2026-09-20 17:51:05 -0300

    Cambie color header de espacios

--- app/(tabs)/espacios/_layout.tsx
+++ app/(tabs)/espacios/_layout.tsx
@@ -1,8 +1,16 @@
 import { Stack } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
 
 export default function EspaciosLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors[colorScheme ?? 'light'].tint },
+        headerTintColor: '#FFFFFF',
+      }}>
       <Stack.Screen name="index" options={{ title: 'Espacios' }} />
     </Stack>
   );
